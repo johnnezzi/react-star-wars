386d56fdaae17ac531de14ac3d993c34000cc4fa
John Nezzi <johnnezzi@yahoo.com>
2020-01-19 11:50:05 +0000

    write get character functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React , { useState , useEffect } from 'react';
+import axios from "axios"
 import './App.css';
 
 function App() {
+  const [ character, setCharacters ] = useState([]);
+
+  useEffect(getPeople,[]);
+
+  function getPeople(event) {
+    if (event) event.preventDefault();
+    axios.get("https://swapi.co/api/people/")
+        .then(res => {
+          console.log(res.data.results);
+          setCharacters(res.data.results);
+        })
+  };
   return (
     <div className="App">
+
 
     </div>
   );
---
+++ src/utils/fetchUtils.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const fetchUtils = {
+
+};
+
+export default fetchUtils;
